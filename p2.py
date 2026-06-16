0d0b12725625abdd0a92eab507244a7c2eadfc93
Vaishnavi85418 <vaishnavichoudhari190@gmail.com>
2026-06-16 16:14:42 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,17 @@
+#list addition of numbers using for loop
+numbers =[1,2,3,4,5,6,7]
+total=0
+for x in numbers:
+    total=total + x
+    print(total)
+
+#set
+
+n = {1,9,7,6}
+total = 0
+for x in n:
+    total = total +x
+    print(total)
+
+
+   
